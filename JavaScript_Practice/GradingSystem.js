7ce1f7dd3d92f5f5843cae739be63ccffd45fbdf
Mitch Fogarty <53394639+MitchFogarty@users.noreply.github.com>
2024-11-14 15:50:44 +1100

    Added function to add a grade to existing student object, and rerun updateStudentStatus

--- JavaScript_Practice/GradingSystem.js
+++ JavaScript_Practice/GradingSystem.js
@@ -29,10 +29,27 @@ const updateStudentStatus = student => {
         student.status = "Fail";
     }
 }
+// Add a new grade to existing student, otherwise return and error if they do not exist. Then update student status with new grade
+const addGrade = (studentName, newGrade) => {
+    const student = students.find(s => s.persName === studentName);
+    if(!student) {
+        console.log("Student not found.");
+        return;
+    }
+    student.grades.push(newGrade);
+    updateStudentStatus(student);
+}
 
 // Code Testing
 addStudent("James", [75, 88, 36]);
 addStudent("Tiffany", [29, 55, 45, 10]);
+addGrade("Marcus", 33);
+students.forEach(student => {
+    console.log(calculateAverage(student));
+    updateStudentStatus(student);
+    console.log(student.status);
+});
+addGrade("Tiffany", 99);
 students.forEach(student => {
     console.log(calculateAverage(student));
     updateStudentStatus(student);
