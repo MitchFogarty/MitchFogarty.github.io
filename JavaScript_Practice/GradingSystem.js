4a824bb3b2eb1836ab142262aaa3992802a558b9
Mitch Fogarty <53394639+MitchFogarty@users.noreply.github.com>
2024-11-14 15:01:42 +1100

    created student array and function to add student object to array

---
+++ JavaScript_Practice/GradingSystem.js
@@ -0,0 +1,14 @@
+let students = [] //empty array to store student objects
+// function to add a student to the students array
+const addStudent = (persName, grades) => {
+    const student = {
+        _persName,
+        _grades,
+        status: "Pending"
+    };
+    students.push(student);
+}
+
+const calculateAverage = student => {
+    
+}
