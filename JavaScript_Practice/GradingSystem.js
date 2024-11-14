b0182328eae30a2c4c75b45cbb59bd38a36e48e0
Mitch Fogarty <53394639+MitchFogarty@users.noreply.github.com>
2024-11-14 15:35:54 +1100

    added function to updates status based on grade average. Created Unit test data

--- JavaScript_Practice/GradingSystem.js
+++ JavaScript_Practice/GradingSystem.js
@@ -2,13 +2,39 @@ let students = [] //empty array to store student objects
 // function to add a student to the students array
 const addStudent = (persName, grades) => {
     const student = {
-        _persName,
-        _grades,
+        persName: persName,
+        grades: grades,
         status: "Pending"
     };
     students.push(student);
 }
 
+//function that is passed student object to calculate grade average
 const calculateAverage = student => {
-    
+    if(student.grades.length === 0) {
+        return "N/A";
+    }
+    let total = student.grades.reduce((acc, grade) => acc + grade, 0);
+    return total / student.grades.length;
 }
+
+//function that is passed a student object to calculate grades status
+const updateStudentStatus = student => {
+    let average = calculateAverage(student);
+    if (average === "N/A") {
+        student.status = 'Pending';
+    } else if (average >= 60) {
+        student.status = "Pass";
+    } else {
+        student.status = "Fail";
+    }
+}
+
+// Code Testing
+addStudent("James", [75, 88, 36]);
+addStudent("Tiffany", [29, 55, 45, 10]);
+students.forEach(student => {
+    console.log(calculateAverage(student));
+    updateStudentStatus(student);
+    console.log(student.status);
+});
